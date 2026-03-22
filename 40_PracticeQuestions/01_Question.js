171deacd94a4a6ffcc241f1061c5d2f204536c57
Vedant Trivedi <vedanttrivedi87@gmail.com>
2026-03-22 17:23:40 +0530

    Added PDF and Screenshots

--- 40_PracticeQuestions/01_Question.js
+++
@@ -1,6 +0,0 @@
-//HOW TO FIND DUPLICATE ARRAY IN JS ?
-//Solution
-    const arrnumber = [1, 2, 3, 4, 2, 4, 1, ]
-    const DUPLICATE = arrnumber.filter((ele, index, arr) => arr.indexOf(ele) !== index)
-    console.log(DUPLICATE);
-    

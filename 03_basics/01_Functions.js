ba70fd92fd2859c405cf7f0ff460e9560defaac1
Vedant Trivedi <vedanttrivedi87@gmail.com>
2026-03-31 21:21:12 +0530

    Add multiplyAndLabel function to perform multiplication
    Added a new function to multiply two numbers and return a labeled result.

--- 03_basics/01_Functions.js
+++ 03_basics/01_Functions.js
@@ -5,3 +5,12 @@ function addtwonumbers(number1, number2){
 // addtwonumbers(4,9)
 const result = addtwonumbers(87,93) //In this we have to define the return type inside the function..
 console.log("Result: ",result);
+
+function multiplyAndLabel(number1, number2) {
+  const product = number1 * number2;
+  return `Product of ${number1} x ${number2} = ${product}`;
+}
+
+const result = multiplyAndLabel(6, 7);
+console.log("Result: ", result);
+// Output: Result:  Product of 6 x 7 = 42
